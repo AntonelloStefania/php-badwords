2deb018c0f75104dd340de5514debf9d88ca6377
AntonelloStefania <antonelloste94@gmail.com>
2023-06-30 15:43:30 +0200

    modifica effetto censura

--- server.php
+++ server.php
@@ -11,6 +11,7 @@ $text_lower= strtolower($text_area)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Censored</title>
 </head>
 <body style="background-color: lightgray">
@@ -19,7 +20,7 @@ $text_lower= strtolower($text_area)
             <div class="col-6 offset-3 my-5">
                 <div class="card p-2 my-5" style='background-color:RGBA(124,255,167,0.8); box-shadow: 0 0 20px 2px black'>
                     <h2 class="text-center fw-bolder">il tuo testo:</h2>
-                    <p class="card py-3 ps-2 mx-3"><?php echo $text_lower ?></p>
+                    <p class="card py-3 ps-2 mx-3"><span class="p-3"><?php echo $text_lower ?></span></p>
                     <div class="d-flex">
                         <span class="ms-2 me-4 fw-bold">lunghezza del tuo testo:</span>
                         <span><?php echo strlen($text_area)?></span>
@@ -27,7 +28,7 @@ $text_lower= strtolower($text_area)
                 </div>
                 <div class="card my-5 p-2" style='background-color:RGBA(124,238,255,0.8);  box-shadow: 0 0 20px 2px black'>
                     <h2 class="text-center fw-bolder">il tuo testo censurato:</h2>
-                    <p class="card py-3 ps-2 mx-3"><?php echo str_replace($lower_censored,"***",$text_lower); ?></p>
+                    <div class="card py-3 ps-2 mx-3 mb-3"><span class="p-3"><?php echo str_replace($lower_censored,"<span style='background-color:black; color:red;' class='ms-2 w-25 fw-lighter px-1'>CENSORED</span>",$text_lower); ?></span></div>
                 </div>
             </div>
         </div>
