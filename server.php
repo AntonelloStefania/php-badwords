862fa80a35bb09ea7d93aec37321538e9788a9dd
AntonelloStefania <antonelloste94@gmail.com>
2023-06-30 15:14:03 +0200

    [add] controllo case-sensitive

--- server.php
+++ server.php
@@ -1,6 +1,8 @@
 <?php
 $censored = $_POST['censored'];
 $text_area= $_POST['textarea'];
+$lower_censored = strtolower($censored);
+$text_lower= strtolower($text_area)
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +19,7 @@ $text_area= $_POST['textarea'];
             <div class="col-6 offset-3 my-5">
                 <div class="card p-2 my-5" style='background-color:RGBA(124,255,167,0.8); box-shadow: 0 0 20px 2px black'>
                     <h2 class="text-center fw-bolder">il tuo testo:</h2>
-                    <p class="card py-3 ps-2 mx-3"><?php echo $text_area ?></p>
+                    <p class="card py-3 ps-2 mx-3"><?php echo $text_lower ?></p>
                     <div class="d-flex">
                         <span class="ms-2 me-4 fw-bold">lunghezza del tuo testo:</span>
                         <span><?php echo strlen($text_area)?></span>
@@ -25,7 +27,7 @@ $text_area= $_POST['textarea'];
                 </div>
                 <div class="card my-5 p-2" style='background-color:RGBA(124,238,255,0.8);  box-shadow: 0 0 20px 2px black'>
                     <h2 class="text-center fw-bolder">il tuo testo censurato:</h2>
-                    <p class="card py-3 ps-2 mx-3"><?php echo str_replace($censored,"***",$text_area) ?></p>
+                    <p class="card py-3 ps-2 mx-3"><?php echo str_replace($lower_censored,"***",$text_lower); ?></p>
                 </div>
             </div>
         </div>
