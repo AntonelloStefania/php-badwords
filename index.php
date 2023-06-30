e379a0ac3a0283b0dd2d19d6dc93a719df8ceedd
AntonelloStefania <antonelloste94@gmail.com>
2023-06-30 14:48:26 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>bad-words</title>
 </head>
-<body>
-    <div class="container">
+<body style="background-color:lightgray">
+    <div class="container pt-5">
         <div class="row mt-5">
-            <div class="col-6 offset-3 card py-3 text-center" style='background-color:RGBA(195,134,245,0.81)'>
-                <h1>Bad Words</h1>
+            <div class="col-6 offset-3 card py-3 text-center" style='background-color:RGBA(195,134,245,0.81);  box-shadow: 0 0 20px 2px black'>
+                <h1 class="fw-bolder text-white">Bad Words</h1>
                 <form action="server.php" method="POST">
                     <div class="my-5">
                         <input type="text" class="form-control"  name="censored" placeholder="scegli la parola da censurare">
@@ -20,7 +20,7 @@
                         <textarea class="w-100 pt-2 ps-2" name="textarea" id="" placeholder="forniscici un testo" ></textarea>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <button type="submit" style='background-color:RGBA(103,146,255,0.9)' class="btn btn-outline-primary">Submit</button>
+                        <button type="submit" style='background-color:RGBA(103,146,255,0.9);  box-shadow: 0 0 5px black; color:white;' class="mt-4 btn btn-outline-primary">Submit</button>
 
                     </div>
                 </form>
--- server.php
+++ server.php
@@ -11,21 +11,21 @@ $text_area= $_POST['textarea'];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Censored</title>
 </head>
-<body>
+<body style="background-color: lightgray">
     <div class="container">
         <div class="row">
             <div class="col-6 offset-3 my-5">
-                <div class="card p-2 my-5" style='background-color:RGBA(124,255,167,0.8)'>
-                    <h3 class="text-center">il tuo testo:</h3>
-                    <p class="card py-3 ps-2"><?php echo $text_area ?></p>
+                <div class="card p-2 my-5" style='background-color:RGBA(124,255,167,0.8); box-shadow: 0 0 20px 2px black'>
+                    <h2 class="text-center fw-bolder">il tuo testo:</h2>
+                    <p class="card py-3 ps-2 mx-3"><?php echo $text_area ?></p>
                     <div class="d-flex">
-                        <span class="ms-2 me-4">lunghezza del tuo testo:</span>
+                        <span class="ms-2 me-4 fw-bold">lunghezza del tuo testo:</span>
                         <span><?php echo strlen($text_area)?></span>
                     </div>
                 </div>
-                <div class="card my-5 p-2" style='background-color:RGBA(124,238,255,0.8)'>
-                    <h3 class="text-center">il tuo testo censurato:</h3>
-                    <p class="card py-3 ps-2"><?php echo str_replace($censored,"***",$text_area) ?></p>
+                <div class="card my-5 p-2" style='background-color:RGBA(124,238,255,0.8);  box-shadow: 0 0 20px 2px black'>
+                    <h2 class="text-center fw-bolder">il tuo testo censurato:</h2>
+                    <p class="card py-3 ps-2 mx-3"><?php echo str_replace($censored,"***",$text_area) ?></p>
                 </div>
             </div>
         </div>
